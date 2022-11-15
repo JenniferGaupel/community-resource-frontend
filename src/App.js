d17f67f0051c27f8168501676361781f9cc2907c
Jennifer Gaupel <jennifergaupel@gmail.com>
2022-11-15 17:15:15 -0500

    Installing material UI, creating ResourceGroups component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import ResourceGroupList from './ResourceGroupList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <ResourceGroupList/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/ResourceGroupList.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+
+const ResourceGroupList = () => {  
+  const [resourceGroups, setResourceGroups] = useState([]);
+
+  var rows = [];
+  
+  // for (resourceGroup in resourceGroups){
+  //   rows.push({id: i, col1: "hello" + i, col2: "world" + i});
+  //   i++;
+  // }
+  resourceGroups.map((rg) => 
+      rows.push({id: rg.id, col1: rg.resource_name, col2: rg.resource_description, col3: rg.main_resources, col4: rg.location})
+  )
+  const columns = [
+    { field: 'col1', headerName: 'Name', width: 150 },
+    { field: 'col2', headerName: 'Description', width: 150 },
+    { field: 'col3', headerName: 'Resources', width: 150 },
+    { field: 'col4', headerName: 'Location', width: 150 }
+  ]; 
+  
+  useEffect(() => {
+    requestResourceGroups();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  
+  async function requestResourceGroups() {
+    const res = await fetch(
+      `http://172.23.0.3/api/v1/resource-groups`
+    );
+    const json = await res.json();
+  
+    setResourceGroups(json.resource_groups);
+  }
+   
+    return (      
+      <div style={{ height: 650, width: '100%' }}>
+        <div>List of Resources:</div>
+        <DataGrid 
+          rows={rows} columns={columns} />
+      </div>
+    );
+  };
+
+  export default ResourceGroupList;
