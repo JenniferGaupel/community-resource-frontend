36fda5c10fbff47f908837dab5cba81d1c836bfa
Jennifer Gaupel <jennifergaupel@gmail.com>
2022-12-12 17:08:30 -0500

    Styling header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import './App.css';
-import ResourceGroupList from './ResourceGroupList';
-import SingleResourceGroup from './SingleResourceGroup'
-import SubmitResource from './SubmitResource'
-import { Link, Route, Routes } from 'react-router-dom'
-import About from './About';
-import Contact from './Contact';
+import "./App.css";
+import ResourceGroupList from "./ResourceGroupList";
+import SingleResourceGroup from "./SingleResourceGroup";
+import SubmitResource from "./SubmitResource";
+import { Link, Route, Routes } from "react-router-dom";
+import About from "./About";
+import Contact from "./Contact";
 
 function App() {
   return (
     <div className="App">
-      <header> 
-        <Link to="/">Community Resource Home Page</Link>
+      <header>
+        <Link id="home-link" to="/">
+          Community Resource Home Page
+        </Link>
         <Link to="/contact">Contact</Link>
-        <Link to="/about">About</Link>               
-      </header>    
+        <Link to="/about">About</Link>
+      </header>
       <Routes>
-        <Route path="/resource/:id" element={<SingleResourceGroup/>}/>
-        <Route path="/submit" element={<SubmitResource/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/" element={<ResourceGroupList/>}/>
-      </Routes>               
+        <Route path="/resource/:id" element={<SingleResourceGroup />} />
+        <Route path="/submit" element={<SubmitResource />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<ResourceGroupList />} />
+      </Routes>
     </div>
   );
 }
